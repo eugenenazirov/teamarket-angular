9c96c975656c4e6aec21b8ab0551ddebff313f80
Eugene Nazirov <jagzonmusic@gmail.com>
2023-05-20 19:26:06 +0600

    Add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {MainComponent} from "./components/pages/main/main.component";
+import {CatalogComponent} from "./components/pages/catalog/catalog.component";
+import {OrderComponent} from "./components/pages/order/order.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MainComponent,
+  },
+  {
+    path: 'catalog',
+    component: CatalogComponent,
+  },
+  {
+    path: 'order',
+    component: OrderComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
