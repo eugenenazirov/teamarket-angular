599884fd2d67b0a57b872f2f03751d8e418f2495
Eugene Nazirov <jagzonmusic@gmail.com>
2023-05-20 19:23:01 +0600

    Add new components to app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,31 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from 'src/app/components/common/header/header.component';
+import {FooterComponent} from './components/common/footer/footer.component';
+import {FormsModule} from "@angular/forms";
+import {MainComponent} from './components/pages/main/main.component';
+import {CatalogComponent} from './components/pages/catalog/catalog.component';
+import {OrderComponent} from './components/pages/order/order.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    MainComponent,
+    CatalogComponent,
+    OrderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
