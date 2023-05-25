abfd68ae0d63197fb67c1ee7b14ecbcb4752ebe5
Eugene Nazirov <jagzonmusic@gmail.com>
2023-05-25 21:21:18 +0600

    Add post request to tea-item service

--- src/app/services/tea-item.service.ts
+++ src/app/services/tea-item.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
-import {TeaItem} from "../components/types/tea-item";
+import {TeaItemType} from "../types/tea-item.type";
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
+import {OrderFormDataType} from "../types/order-form-data.type";
+import {OrderResponseType} from "../types/order-response.type";
 
 @Injectable()
 export class TeaItemService {
@@ -9,11 +11,15 @@ export class TeaItemService {
     private http: HttpClient,
   ) { }
 
-  public getTeaItems(): Observable<TeaItem[]> {
-    return this.http.get<TeaItem[]>('https://testologia.site/tea');
+  public getTeaItems(): Observable<TeaItemType[]> {
+    return this.http.get<TeaItemType[]>('https://testologia.site/tea');
   }
 
-  public getTeaItem(id: number): Observable<TeaItem> {
-    return this.http.get<TeaItem>('https://testologia.site/tea', {params: {id: id.toString()}});
+  public getTeaItem(id: number): Observable<TeaItemType> {
+    return this.http.get<TeaItemType>('https://testologia.site/tea', {params: {id: id.toString()}});
+  }
+
+  public orderTeaItem(orderFormData: OrderFormDataType): Observable<OrderResponseType> {
+    return this.http.post<OrderResponseType>('https://testologia.site/order-tea', orderFormData);
   }
 }
